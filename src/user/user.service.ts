6605f88a09469527e4924d42d33d808385ec60c3
ReganHe <heoo547627@126.com>
2024-09-20 17:22:10 +0800

    增加校验和注释

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,10 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
+  /**
+   * 获取指定范围的数组
+   * @param max 数组最大值
+   * @returns 数组
+   */
   getRange(max: number): any {
-    var result = [];
+    if (max < 0) {
+      return {
+        status: 1001,
+        data: [],
+        message: 'Invalid input',
+      };
+    }
+
+    var result: string[] = [];
     for (var i = 1; i <= max; i++) {
-      result.push(i);
+      result.push(i.toString());
     }
     return {
       status: 0,
